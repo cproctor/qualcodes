644c5c30ba214a21921e403622226f29aaf03825
Chris Proctor <chris.proctor@gmail.com>
2016-12-03 15:07:03 -0800

    Adding csv output

--- codes/models.py
+++ codes/models.py
@@ -14,3 +14,15 @@ class Artifact(models.Model):
     codes = models.ManyToManyField(Code, related_name='artifacts')
     user_id = models.CharField('User ID', max_length=200)
     column_id = models.CharField('Column ID', max_length=200)
+
+    # Wish we had users.
+    @classmethod
+    def user_ids(cls):
+        user_ids = set()
+        for a in Artifact.objects.all():
+            user_ids.add(a.user_id)
+        return user_ids
+
+    def code_dict(self):
+        return {"{}_{}".format(self.column_id, code): int(code in self.codes.all()) for code in Code.objects.all()}
+        
--- codes/views.py
+++ codes/views.py
@@ -3,7 +3,10 @@ from rest_framework import viewsets, status
 from codes.models import Code, Artifact
 from codes.serializers import CodeSerializer, ArtifactSerializer
 from django.shortcuts import get_object_or_404, render
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import detail_route, list_route
+from django.http import HttpResponse
+import csv
+import io
 
 class CodeViewSet(viewsets.ModelViewSet):
     queryset = Code.objects.all()
@@ -12,6 +15,21 @@ class CodeViewSet(viewsets.ModelViewSet):
 class ArtifactViewSet(viewsets.ModelViewSet):
     queryset = Artifact.objects.all()
     serializer_class = ArtifactSerializer
+
+    @list_route(methods=['get'])
+    def csv(self, request):
+        users = {uid: {'user_id': uid} for uid in Artifact.user_ids()}
+        for artifact in Artifact.objects.all():
+            users[artifact.user_id].update(artifact.code_dict())
+
+        output = io.StringIO()
+        writer = csv.DictWriter(output, users[list(Artifact.user_ids())[0]].keys()) 
+        writer.writeheader()
+        for user, codes in users.items():
+            writer.writerow(codes)
+        output.seek(0)
+        result = output.read()
+        return HttpResponse(result, content_type="text/csv")
 
     @detail_route(methods=['get'])
     def code(self, request, pk):
